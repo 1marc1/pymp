fb548eed0a228e38741b0150d7eda63ccd9ec0cc
1marc1 <1marc1@gmail.com>
2020-10-15 00:57:05 +1100

    Added listener configurer and process functions

--- pymp.py
+++ pymp.py
@@ -32,6 +32,27 @@ import multiprocessing
 import pymp_global as gv
 import pymp_common as dc
 
+def listener_configurer():
+    root = logging.getLogger()
+    h = logging.handlers.RotatingFileHandler('mptest.log', 'a', 300, 10)
+    f = logging.Formatter('%(asctime)s %(processName)-10s %(name)s %(levelname)-8s %(message)s')
+    h.setFormatter(f)
+    root.addHandler(h)
+
+def listener_process(queue, configurer):
+    configurer()
+    while True:
+        try:
+            record = queue.get()
+            if record is None:  # We send this as a sentinel to tell the listener to quit.
+                break
+            logger = logging.getLogger(record.name)
+            logger.handle(record)  # No level or filter logic applied - just do it!
+        except Exception:
+            import sys, traceback
+            print('Whoops! Problem:', file=sys.stderr)
+            traceback.print_exc(file=sys.stderr)
+
 def init_log():
     _logfile = os.path.join(tempfile.gettempdir(),gv.GeneralLogFileName)
     gv.logger = logging.getLogger('pymp')
